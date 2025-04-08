592855d5275f3ff7d8ef2a572d9cc81c565b4d3a
abyss9x3 <103586132+abyss9x3@users.noreply.github.com>
2025-04-08 23:44:32 +0530

    Update CursorEffect.js

--- src/components/CursorEffect.js
+++ src/components/CursorEffect.js
@@ -14,10 +14,22 @@ const CursorEffect = () => {
       });
     };
 
+    const handleHover = e => {
+      if (e.target.closest('a, button')) {
+        cursor.classList.add('custom-cursor-hover');
+      } else {
+        cursor.classList.remove('custom-cursor-hover');
+      }
+    };
+
     window.addEventListener('mousemove', moveCursor);
+    window.addEventListener('mouseover', handleHover);
+    window.addEventListener('mouseout', handleHover);
 
     return () => {
       window.removeEventListener('mousemove', moveCursor);
+      window.removeEventListener('mouseover', handleHover);
+      window.removeEventListener('mouseout', handleHover);
       document.body.removeChild(cursor);
     };
   }, []);
